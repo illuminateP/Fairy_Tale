b7db739641f7f09f2de6e06ee97dc3404021919e
illuminateP <sincelife777@gmail.com>
2025-07-22 19:09:12 -1000

    ver 1.0

--- src/run.py
+++
@@ -1,5 +0,0 @@
-from .utils.app import create_app # app.py에서 create_app 함수를 가져옵니다.
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)  # 개발 중에는 debug=True 사용, 포트 변경 가능 (예: port=5001) 
